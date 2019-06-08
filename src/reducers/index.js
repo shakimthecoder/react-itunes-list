c5cc42328e6b4956cf0eeef3d028100235ca9633
amenraprod <amenraprod@gmail.com>
2019-06-08 13:51:54 -0400

    added space and arrow

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,23 +2,20 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
     return [
-        {
             { title: 'Love on the Brain', duration: '3:40' },
             { title: 'Suge', duration: '4:55' },
             { title: 'Big Ole Freak', duration: '4:44' },
             { title: 'Hello', duration: '5:55' }
-
-        };
     ];
+};
 
-    const selectedSongReducer = (selectedSong=null, action ) {
+    const selectedSongReducer = (selectedSong = null, action ) => {
     if (action.type === 'SONG_SELECTED') {
         return action.payload;
     }
-
     return selectedSong;
    
-        };
+
 
     export default combineReducers({
         song: songsReducer,
