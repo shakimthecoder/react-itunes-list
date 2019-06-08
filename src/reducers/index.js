e58cca2dae847475f2f3b75a155ab9c807e20242
amenraprod <amenraprod@gmail.com>
2019-06-08 13:11:38 -0400

    created reducer for song selection and list

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        {
+            { title: 'Love on the Brain' },
+            { title: 'Suge' },
+            { title: 'Big Ole Freak' },
+            { title: 'Hello' }
+
+        };
+    ];
+
+    const selectedSongReducer = (selectedSong=null, action ) {
+
+    };
+};
