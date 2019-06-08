e01505e2a9f174968bc8484e981cb449643f21fa
amenraprod <amenraprod@gmail.com>
2019-06-08 13:16:19 -0400

    added if/else statement to verify song selection

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,20 @@
 const songsReducer = () => {
     return [
         {
-            { title: 'Love on the Brain' },
-            { title: 'Suge' },
-            { title: 'Big Ole Freak' },
-            { title: 'Hello' }
+            { title: 'Love on the Brain', duration: '3:40' },
+            { title: 'Suge', duration: '4:55' },
+            { title: 'Big Ole Freak', duration: '4:44' },
+            { title: 'Hello', duration: '5:55' }
 
         };
     ];
 
     const selectedSongReducer = (selectedSong=null, action ) {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
 
-    };
+    return selectedSong;
+   
+        };
 };
