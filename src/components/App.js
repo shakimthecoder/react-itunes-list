b0516c4ce1dd1214be495f5efe45a0b44355ca9e
amenraprod <amenraprod@gmail.com>
2019-06-08 12:43:57 -0400

    test

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render()
+    {
+        return(
+            <div>Homepage</div>
+        )
+    }
+};
+
+export default App;
